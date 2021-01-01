587314914860e188d7840cfa826d0b30b7f3c943
晏利军 <yanlijun1000@gmail.com>
2021-01-02 05:54:24 +0800

    add cases

---
+++ cases/test/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>晏利军 | 晏利军的网站 | 晏利军的个人网站 | 空页面</title>
+<meta name="keywords" content="晏利军,前端,web前端,前端开发,web前端开发,前端技术,web前端技术,前台,网页制作,css3,html5,jQuery,css3+html5,css3+html5+jQuery,后端,web后端,后端开发,web后端开发,php">
+<meta name="description" content="晏利军，从事web前端开发工作，制作晏利军的个人网站，是为学习最新web前端开发技术，CSS3、HTML5、jQuery，希望能与更多前端开发者或爱好者，互相学习，互相借鉴，互相提高。">
+<meta name="copyright" content="Copyright © 2013 YANLIJUN.All Rights Reserved">
+<meta name="author" content="晏利军 &amp; Kevin Yan">
+<meta name="robots" content="all">
+<meta property="qc:admins" content="5045420106412566375">
+<noscript><meta http-equiv="refresh" content="0; url=http://yanlijun.com/nojs.html"></noscript>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+<h1>Nginx 测试</h1>
+</body>
+</html>
